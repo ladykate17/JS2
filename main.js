3d9f2ebf01463513a74cfc94dbd2bbcccb32e054
ladykate17 <stamm.katherine@gmail.com>
2015-06-22 15:32:02 -0600

    completed exercise

---
+++ main.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var userWord = prompt("Please enter a word.");
+alert(userWord + ", is a great choice!");
+
+	console.log("Entered Word: " + userWord);
+	console.log("Entered Word Length: " + userWord.length);
+	console.log("The 3rd character of the word is: " + userWord.charAt(2));
+	console.log("Lowercase: " + userWord.toLowerCase());
+	console.log("Uppercase: " + userWord.toUpperCase());
+	console.log("You picked the the word, " + userWord + ". Now, isn't that a nifty word!");
+	console.log("The 2nd through 4th characters of the word are: " + userWord.substr(1, 3));
+
+	alert("Entered Word: " + userWord);
+	alert("Entered Word Length: " + userWord.length);
+	alert("The 3rd character of the word is: " + userWord.charAt(2));
+	alert("Lowercase: " + userWord.toLowerCase());
+	alert("Uppercase: " + userWord.toUpperCase());
+	alert("You picked the the word, " + userWord + ". Now, isn't that a nifty word!");
+	alert("The 2nd through 4th characters of the word are: " + userWord.substr(1, 3));
